401a62ea9aaa049064e42f2f4f7f9bd4371b16cb
jhangkar <taqit3852@gmail.com>
2022-12-24 06:16:00 +0600

    loader final

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,13 +3,15 @@ import React from 'react';
 const Loader = () => {
     return (
         <div className='flex justify-center items-center my-16'>
-               <>
+            
                    
               
-                        <div className="w-24 h-24 rounded-full animate-spin
-                    border-y-4 border-double border-gray-700 border-t-transparent"></div>
+                        <div className="w-24 h-24 rounded-full animate-spin 
+                    border-y-4 border-double border-[#f7f7f75b] border-t-transparent ">
+                    </div>
+                        <h1 className='font-extrabold text-[#f7f7f7c5] text-4xl absolute animate-bounce '>T</h1>
                     
-                </>
+                
             </div>
       
     );
